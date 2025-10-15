e65fc790cb687772979f8a913aebacc6688f5d84
ozgeisler <ozgeisler00@gmail.com>
2025-10-15 18:20:42 +0300

    Adding multilication

--- multplictionfunction.m
+++ multplictionfunction.m
@@ -1,11 +1,11 @@
-% Time interval
+on% Time interval
 t = (1910:10:2000)';
 
-% Multipl
+% Populationn
 p = [91.972 105.711 123.203 131.669 150.697...
     179.323 203.212 226.505 249.633 281.422]';
 
-% Plot
+% Multiplication
 plot(t,p,'bo');
 axis([1910 2020 0 400]);
 title('Population of the US 1910-2000');
