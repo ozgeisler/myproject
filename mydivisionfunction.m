b1c26154634059f901d049a03c6bc843c9a84bcc
ozgeisler <ozgeisler00@gmail.com>
2025-10-16 01:44:06 +0300

    Addind Division

---
+++ mydivisionfunction.m
@@ -0,0 +1,13 @@
+on% Time interval
+t = (1910:10:2000)';
+
+% Populationn
+p = [91.972 105.711 123.203 131.669 150.697...
+    179.323 203.212 226.505 249.633 281.422]';
+
+% Division 
+plot(t,p,'bo');
+axis([1910 2020 0 400]);
+title('Population of the US 1910-2000');
+ylabel('Millions');
+ %divison flan asgshdghsd 
